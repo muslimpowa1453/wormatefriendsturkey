af47ab8a47c6d4374e8fd69032b24cc35da2a532
muslimpowa1453 <muslimpowa8@gmail.com>
2025-08-13 14:05:52 +0300

    Add files via upload

---
+++ check/check2.php
@@ -0,0 +1,376 @@
+{
+  "streamer": "WFT",
+  "extension": "teamwft",
+  "game": "Wormate.io",
+  "note": "Please don't copy my code",
+  "programmer": "TeaWFT",
+  "e": "empty",
+  "z": "c",
+  "s11": "0.0.0.0.0.0.0.0.0.0.0",
+  "sw": 1,
+  "tt": 1,
+  "vs": 42,
+  "dsg": [],
+  "propertyList": [
+    {
+      "id": 386,
+      "type": "SKIN"
+    },
+    {
+      "id": 387,
+      "type": "SKIN"
+    },
+    {
+      "id": 388,
+      "type": "SKIN"
+    },
+    {
+      "id": 394,
+      "type": "SKIN"
+    },
+    {
+      "id": 405,
+      "type": "SKIN"
+    },
+    {
+      "id": 479,
+      "type": "SKIN"
+    },
+    {
+      "id": 480,
+      "type": "SKIN"
+    },
+    {
+      "id": 481,
+      "type": "SKIN"
+    },
+    {
+      "id": 482,
+      "type": "SKIN"
+    },
+    {
+      "id": 483,
+      "type": "SKIN"
+    },
+    {
+      "id": 484,
+      "type": "SKIN"
+    },
+    {
+      "id": 485,
+      "type": "SKIN"
+    },
+    {
+      "id": 486,
+      "type": "SKIN"
+    },
+    {
+      "id": 487,
+      "type": "SKIN"
+    },
+    {
+      "id": 488,
+      "type": "SKIN"
+    },
+    {
+      "id": 489,
+      "type": "SKIN"
+    },
+    {
+      "id": 490,
+      "type": "SKIN"
+    },
+    {
+      "id": 523,
+      "type": "SKIN"
+    },
+    {
+      "id": 550,
+      "type": "SKIN"
+    },
+    {
+      "id": 551,
+      "type": "SKIN"
+    },
+    {
+      "id": 552,
+      "type": "SKIN"
+    },
+    {
+      "id": 554,
+      "type": "SKIN"
+    },
+    {
+      "id": 556,
+      "type": "SKIN"
+    },
+    {
+      "id": 560,
+      "type": "SKIN"
+    },
+    {
+      "id": 562,
+      "type": "SKIN"
+    },
+    {
+      "id": 563,
+      "type": "SKIN"
+    },
+    {
+      "id": 564,
+      "type": "SKIN"
+    },
+    {
+      "id": 565,
+      "type": "SKIN"
+    },
+    {
+      "id": 569,
+      "type": "SKIN"
+    },
+    {
+      "id": 590,
+      "type": "SKIN"
+    },
+    {
+      "id": 599,
+      "type": "SKIN"
+    },
+    {
+      "id": 617,
+      "type": "SKIN"
+    },
+    {
+      "id": 629,
+      "type": "SKIN"
+    },
+    {
+      "id": 648,
+      "type": "SKIN"
+    },
+    {
+      "id": 649,
+      "type": "SKIN"
+    },
+    {
+      "id": 684,
+      "type": "SKIN"
+    },
+    {
+      "id": 685,
+      "type": "SKIN"
+    },
+    {
+      "id": 686,
+      "type": "SKIN"
+    },
+    {
+      "id": 687,
+      "type": "SKIN"
+    },
+    {
+      "id": 688,
+      "type": "SKIN"
+    },
+    {
+      "id": 730,
+      "type": "SKIN"
+    },
+    {
+      "id": 731,
+      "type": "SKIN"
+    },
+    {
+      "id": 732,
+      "type": "SKIN"
+    },
+    {
+      "id": 733,
+      "type": "SKIN"
+    },
+    {
+      "id": 734,
+      "type": "SKIN"
+    },
+    {
+      "id": 744,
+      "type": "SKIN"
+    },
+    {
+      "id": 750,
+      "type": "SKIN"
+    },
+    {
+      "id": 751,
+      "type": "SKIN"
+    },
+    {
+      "id": 754,
+      "type": "SKIN"
+    },
+    {
+      "id": 770,
+      "type": "SKIN"
+    },
+    {
+      "id": 771,
+      "type": "SKIN"
+    },
+    {
+      "id": 772,
+      "type": "SKIN"
+    },
+    {
+      "id": 773,
+      "type": "SKIN"
+    },
+    {
+      "id": 774,
+      "type": "SKIN"
+    },
+    {
+      "id": 775,
+      "type": "SKIN"
+    },
+    {
+      "id": 776,
+      "type": "SKIN"
+    },
+    {
+      "id": 777,
+      "type": "SKIN"
+    },
+    {
+      "id": 778,
+      "type": "SKIN"
+    },
+    {
+      "id": 779,
+      "type": "SKIN"
+    },
+    {
+      "id": 798,
+      "type": "SKIN"
+    },
+    {
+      "id": 799,
+      "type": "SKIN"
+    },
+    {
+      "id": 800,
+      "type": "SKIN"
+    },
+    {
+      "id": 801,
+      "type": "SKIN"
+    },
+    {
+      "id": 802,
+      "type": "SKIN"
+    },
+    {
+      "id": 807,
+      "type": "SKIN"
+    },
+    {
+      "id": 808,
+      "type": "SKIN"
+    },
+    {
+      "id": 809,
+      "type": "SKIN"
+    },
+    {
+      "id": 429,
+      "type": "HAT"
+    },
+    {
+      "id": 430,
+      "type": "HAT"
+    },
+    {
+      "id": 431,
+      "type": "HAT"
+    },
+    {
+      "id": 451,
+      "type": "HAT"
+    },
+    {
+      "id": 452,
+      "type": "HAT"
+    },
+    {
+      "id": 776,
+      "type": "HAT"
+    },
+    {
+      "id": 778,
+      "type": "HAT"
+    },
+    {
+      "id": 779,
+      "type": "HAT"
+    },
+    {
+      "id": 780,
+      "type": "HAT"
+    },
+    {
+      "id": 781,
+      "type": "HAT"
+    },
+    {
+      "id": 782,
+      "type": "HAT"
+    }
+  ],
+  "cc": "\u003Cstyle\u003E\n    \u003C/style\u003E\u003Cdiv id=\"div_map\"\u003E\u003C/div\u003E\u003Cp id=\"update_new_2\" style=\"text-align: center;position: absolute;bottom: -15px;width: 100%;background-image: linear-gradient(#ffffff, #ffffff);font-weight: bold;color: black;border: 1px solid;border-radius: 10px;border-color: #ffffff;\"\u003Eمن فضلك ، امسح سجل المتصفح لتحديث الاضافة. شكرًا.\u003C/p\u003E",
+  "mb": "\u003Cdiv class='info-message' style='background-color: rgba(255, 204, 0, 0.1); border-radius: 8px; padding: 15px; margin: 10px 0; border: 1px solid rgba(255, 204, 0, 0.3);'\u003E\u003Ch4 class='center' style='color: #ffbb00; margin-top: 0;'\u003Eملاحظة مهمة\u003C/h4\u003E\u003Cp style='text-align: right; direction: rtl; margin-bottom: 10px;'\u003Eيمكنك زيادة سرعة الزوم والتصغير من خلال زيارة قسم \"حجم الادوات والزوم\" وضبط شريط تمرير سرعة الزوم للحصول على تجربة لعب أفضل.\u003C/p\u003E\u003Cp style='text-align: left; direction: ltr; margin-bottom: 0;'\u003EYou can increase the zoom speed by visiting the \"Power-ups & Zoom\" section and adjusting the zoom speed slider for a better gaming experience.\u003C/p\u003E\u003C/div\u003E",
+  "cm": "",
+  "cr": "",
+  "ccc": "iq",
+  "ccg": [
+    "Will Kill and Headshot statistics be removed?",
+    "There was a connection problem!",
+    "Your account has been locked.",
+    "RESPAWN",
+    "Full Screen",
+    "In the game",
+    "⚙️ Settings",
+    "ID",
+    "ID COPY",
+    "COPY ID",
+    "Country",
+    "Remember kill and headshot counts",
+    "ON/OFF",
+    "Background image",
+    "COPY",
+    "Vietnam",
+    "Thailand",
+    "Cambodia",
+    "Indonesia",
+    "Singapore",
+    "Japan",
+    "Mexico",
+    "Brazil",
+    "Canada",
+    "Germany",
+    "France",
+    "England",
+    "Australia",
+    "USA",
+    "Portugal",
+    "Shortcut",
+    "Dark blue sky",
+    "Purple sky",
+    "Clouds",
+    "💡",
+    "Turkey",
+    "Arabs"
+  ],
+  "is_subscriber": false,
+  "show_discord": true,
+  "discord_button": "\n\u003Cdiv id=\"discord-link-container\" style=\"margin-top: 20px; text-align: center;\"\u003E\n  \u003Ca href=\"https://discord.gg/your-discord-link\" target=\"_blank\" style=\"display: inline-block; background-color: #5865F2; color: white; padding: 8px 16px; text-decoration: none; border-radius: 4px; font-weight: bold;\"\u003E\n    \u003Cimg src=\"https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_white_RGB.png\" alt=\"Discord\" style=\"height: 20px; vertical-align: middle; margin-right: 8px;\"\u003E\n    انضم إلى مجتمعنا على Discord\n  \u003C/a\u003E\n  \u003Cp style=\"margin-top: 10px; font-size: 13px; color: #666;\"\u003Eللحصول على ميزات إضافية، انضم إلى سيرفر Discord واشترك في النسخة المميزة\u003C/p\u003E\n\u003C/div\u003E\n",
+  "server_time": "2025-05-23 06:43:41"
+}
